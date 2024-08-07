89c5f54d4e8a49af0a0456129c07661b24dd772f
kulaizki <kulaizke@gmail.com>
2024-08-07 13:28:35 +0800

    feat: add test component

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+import Flex from "@/components/Flex";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+    <Flex />
+    </View>
+  );
+}
---
+++ components/Flex.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Flex = () => {
+  return (
+    <View>
+      <Text>Test component</Text>
+    </View>
+  )
+}
+
+export default Flex;
