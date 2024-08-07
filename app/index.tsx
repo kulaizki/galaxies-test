a2baba856e8016aa92662a5056240fb36065862f
kulaizki <kulaizke@gmail.com>
2024-08-07 15:07:49 +0800

    refactor: add default platform component

--- app/index.tsx
+++ app/index.tsx
@@ -2,13 +2,15 @@ import { View, StyleSheet } from "react-native";
 import Conditional from "@/components/Conditional";
 import Functions from "@/components/Functions";
 import PlatformStyle from "@/components/PlatformStyle";
+import Welcome from "@/components/welcome/welcome";
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      {/* <Functions />
-      <Conditional /> */}
-      <PlatformStyle /> 
+      {/* <Functions /> */}
+      {/* <Conditional />  */}
+      {/* <PlatformStyle />  */}
+      <Welcome />
     </View>
   );
 }
